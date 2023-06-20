d99acc3e214c98a6d4950e4c6f01e846d3a0dd07
bezabihbekele0875 <bezabihbekele0875@gmail.com>
2023-06-20 09:59:05 -0700

    project

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,8 +8,8 @@
 void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
+void times_table(void);
+void print_to_98(int n);
 void print_times_table(int);
 int _putchar(char);
 int _islower(int c);
